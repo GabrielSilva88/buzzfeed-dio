5a509f219efe6588ca869b434b8d4854680946f5
Gabriel Silva <gabrielmg.silva@outlook.com>
2025-02-20 19:06:29 -0300

    "Finalizando projeto buzzfeed"

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+  bootstrapApplication(AppComponent).catch((err) => console.error(err));
